daeca8a29c1a83fc42696273fd4847a29270c666
Chris <christopher.p.tran@gmail.com>
2023-07-31 15:43:34 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useReducer} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
 }
 
-export default App;
+function reducer(state, action) {
+  switch(action.type) {
+    case ACTIONS.INCREMENT:
+      return {count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count -1}
+    default:
+      return state
+  }
+
+  
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+  
+
+  function increment() {
+    dispatch({type: ACTIONS.INCREMENT})
+  }
+
+  function decrement() {
+    dispatch({type: ACTIONS.DECREMENT})
+  }
+
+
+  return (
+    <>
+    <button onClick={decrement}>-</button>
+    <span>{state.count}</span>
+    <button onClick={increment}>+</button>
+    </>
+  )
+
+
+}
+
